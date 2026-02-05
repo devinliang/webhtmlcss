3b682320101e6fdd7baf13ff1d0304e79728b820
Devin Liang <devinliang@gmail.com>
2026-02-05 19:14:06 +0800

    Revise vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
@@ -9,6 +10,12 @@ export default defineConfig({
   publicDir: '../public',
   build: {
     outDir: '../dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        www: resolve(__dirname, 'src/www.html'),
+      },
+    },
   },
   // 針對 GitHub Pages 的路徑設定，替換為您的儲存庫名稱
   base: '/webhtmlcss/', 
