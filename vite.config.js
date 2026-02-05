8ddfa127e604e134b11bff70c5eb93af20143274
Devin Liang <devinliang@gmail.com>
2026-02-05 18:39:24 +0800

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  root: 'src',
+  build: {
+    outDir: '../dist',
+  },
   // 針對 GitHub Pages 的路徑設定，替換為您的儲存庫名稱
   base: '/webhtmlcss/', 
 })
